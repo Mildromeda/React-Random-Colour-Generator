92298d10ceb83988ec3e70f3d9a95ebe54bec6aa
Kiran <ayyalas@protonmail.com>
2018-08-16 14:38:22 +0530

    v1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from './Button'
+import Random from './Random'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Random/>
+      </div>
+    );
+  }
+}
+
+export default App;
